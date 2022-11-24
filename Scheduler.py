2ceec976324950d2ac5e2238b8c1247d3435ab1b
Remco <remco.aj.janssen@kpn.com>
2022-11-24 18:10:44 +0100

    Scheduler

--- Scheduler.py
+++ Scheduler.py
@@ -1,20 +1,16 @@
-import os
 import schedule
+import time
+import os
 from Database import check_result_send_mess
 
-ON_HEROKU = os.environ.get('ON_HEROKU')
-start_time = os.environ.get('start_time')
 minutes = os.environ.get('minutes')
-
-if ON_HEROKU:
-    # get the heroku port
-    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
-else:
-    port = 3000
+print('Minutes variable set to: ' + minutes)
 
 # schedule crawler
-schedule.every(5).minutes.do(check_result_send_mess)
+schedule.every(1).minutes.do(check_result_send_mess)
+print('Scheduled..')
 
 # run script infinitely
 while True:
     schedule.run_pending()
+    time.sleep(1)
