c4f95e8b00d703ea80a71592a1b9727d68f97ecb
Remco <remco.aj.janssen@kpn.com>
2022-11-24 16:50:07 +0100

    Changed schedule.

--- Scheduler.py
+++ Scheduler.py
@@ -4,6 +4,7 @@ from Database import check_result_send_mess
 
 ON_HEROKU = os.environ.get('ON_HEROKU')
 start_time = os.environ.get('start_time')
+minutes = os.environ.get('minutes')
 
 if ON_HEROKU:
     # get the heroku port
@@ -12,7 +13,7 @@ else:
     port = 3000
 
 # schedule crawler
-schedule.every().day.at(start_time).do(check_result_send_mess)
+schedule.every(5).minutes.do(check_result_send_mess)
 
 # run script infinitely
 while True:
