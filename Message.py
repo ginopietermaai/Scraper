2752e8e4791f317a404965bd15e3078867448e93
Remco Janssen <remco.janssen@truelime.nl>
2022-11-27 15:18:11 +0100

    Now.

--- Message.py
+++ Message.py
@@ -1,13 +1,13 @@
 import requests
 import html_to_json
-from bs4 import BeautifulSoup
+import unicodedata
 
 def send_message(chat_id, text):
     bot = 'https://api.telegram.org/bot5980362577:AAGMqqy4nSZPrxdL6KLboDalAE6DQRJ3z2o/'
     
     # parse message 
     json = str(html_to_json.convert_tables(text))
-    cleanjson = BeautifulSoup(json, 'lmxl').get_text(strip=True)
+    cleanjson = unicodedata.normalize("NFKD", json)
 
     # split and send message to telegram
     for i in range(0, len(cleanjson), 4000):
