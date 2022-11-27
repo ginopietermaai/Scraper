de396dd45fc2c22869ad2fc1c27b4a155e5f499e
Remco Janssen <remco.janssen@truelime.nl>
2022-11-27 15:05:17 +0100

    Fix.

--- Message.py
+++ Message.py
@@ -5,7 +5,7 @@ def send_message(chat_id, text):
     bot = 'https://api.telegram.org/bot5980362577:AAGMqqy4nSZPrxdL6KLboDalAE6DQRJ3z2o/'
     
     # parse message 
-    json = str(html_to_json.convert_tables(text)).replace('\xa0', '')
+    json = str(html_to_json.convert_tables(text)).replace('/xa0', '')
 
     # split and send message to telegram
     for i in range(0, len(json), 4000):
