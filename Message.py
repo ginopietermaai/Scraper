f6f1e9865be547a5f733d7c54c68eb945366dc53
Remco Janssen <remco.janssen@truelime.nl>
2022-11-27 15:25:13 +0100

    NC.

--- Message.py
+++ Message.py
@@ -6,11 +6,10 @@ def send_message(chat_id, text):
     bot = 'https://api.telegram.org/bot5980362577:AAGMqqy4nSZPrxdL6KLboDalAE6DQRJ3z2o/'
     
     # parse message 
-    json = str(html_to_json.convert_tables(text))
-    cleanjson = unicodedata.normalize("NFKD", json)
+    json = str(html_to_json.convert_tables(text).replace('\\xa0', ' '))
 
     # split and send message to telegram
-    for i in range(0, len(cleanjson), 4000):
-     parameters = {'chat_id': chat_id, 'text': cleanjson[i:i+4000]}
+    for i in range(0, len(json), 4000):
+     parameters = {'chat_id': chat_id, 'text': json[i:i+4000]}
      requests.post(bot + 'sendMessage', data=parameters)
   
