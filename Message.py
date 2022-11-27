b182151538c442a3fb0882db6b1b1697699d1e92
Remco Janssen <remco.janssen@truelime.nl>
2022-11-27 17:17:19 +0100

    No json anymore, bye bye.

--- Message.py
+++ Message.py
@@ -1,15 +1,10 @@
 import requests
-import html_to_json
-import unicodedata
 
 def send_message(chat_id, text):
     bot = 'https://api.telegram.org/bot5980362577:AAGMqqy4nSZPrxdL6KLboDalAE6DQRJ3z2o/'
     
-    # parse message 
-    json = str(html_to_json.convert_tables(text)).replace('\\xa0', '').replace('"', '')
-
     # split and send message to telegram
-    for i in range(0, len(json), 4000):
-     parameters = {'chat_id': chat_id, 'text': json[i:i+4000]}
+    for i in range(0, len(text), 4000):
+     parameters = {'chat_id': chat_id, 'text': text[i:i+4000]}
      requests.post(bot + 'sendMessage', data=parameters)
   
