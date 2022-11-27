c4694207824a7001b0379fde30f36a7c14bfc57d
Remco Janssen <remco.janssen@truelime.nl>
2022-11-27 14:41:03 +0100

    Convert table.

--- Message.py
+++ Message.py
@@ -5,7 +5,7 @@ def send_message(chat_id, text):
     bot = 'https://api.telegram.org/bot5980362577:AAGMqqy4nSZPrxdL6KLboDalAE6DQRJ3z2o/'
     
     # parse message 
-    json = html_to_json(text)
+    json = html_to_json.convert_html_tables(text)
 
     # split and send message to telegram
     for i in range(0, len(json), 4000):
